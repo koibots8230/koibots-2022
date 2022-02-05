9fb6bb0d6426dd4aebab2f9e1bee700958a2570c
Alex <alexjfuhr@gmail.com>
2022-02-05 13:10:49 -0500

    Feat: Added drivetrain subsystem

--- newRobot/src/main/java/frc/robot/Robot.java
+++ newRobot/src/main/java/frc/robot/Robot.java
@@ -4,15 +4,11 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ExampleCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
---
+++ newRobot/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrainSubsystem extends SubsystemBase {
+  private final XboxController xBoxController = new XboxController(0);
+  private final VictorSPX frontRightMotor = new VictorSPX(0);
+  private final VictorSPX backRightMotor = new VictorSPX(0);
+  private final VictorSPX frontLeftMotor = new VictorSPX(0);
+  private final VictorSPX backLeftMotor = new VictorSPX(0);
+
+  /** Creates a new ExampleSubsystem. */
+  public DriveTrainSubsystem() {
+    backRightMotor.follow(frontRightMotor);
+    backRightMotor.setInverted(false);
+    backLeftMotor.follow(frontLeftMotor);
+    backLeftMotor.setInverted(false);
+  }
+
+  @Override
+  public void periodic() {
+    frontLeftMotor.set(ControlMode.PercentOutput, xBoxController.getRawAxis(XboxController.Axis.kLeftY.value));
+    frontRightMotor.set(ControlMode.PercentOutput, xBoxController.getRawAxis(XboxController.Axis.kRightY.value));
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
