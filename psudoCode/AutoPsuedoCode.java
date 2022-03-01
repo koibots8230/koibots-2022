aa76bdfd868b5629fe4237d8cf603576b98104d5
Alex Anderson <chaoticgamer.exe@gmail.com>
2022-03-01 17:24:14 -0500

    pseudo code for autonomus submitted

---
+++ psudoCode/AutoPsuedoCode.java
@@ -0,0 +1,42 @@
+import edu.wpi.first.wpilibj.Timer;
+
+public class AutoPsuedoCode{
+  //AUTONOMOUS PHASE
+  //DRIVE TO POSITION
+  RobotTimer = Timer();
+
+  RobotTimer.start() //starts a timer at 0 seconds
+    while(isAutonomous() && isEnabled()) //run when autonomous is Clive and you are enabled
+    {
+        if(RobotTimer.get() < timeToGetToPosition) //time is a double in terms of seconds
+        {
+            driveTrain.set(speed); //move robot at this speed with/without curve
+        }
+        //Timer.delay(0.005); //does nothing for 5 seconds but helps refresh motors in loop
+    }
+    RobotTimer.stop() //stops timer
+
+    //SHOOT WHEN AT POSITION
+    while(isAutonomous() && isEnabled()) //run when autonomous is Clive and you are enabled
+    {
+        if(RobotTimer.get() < avgTimeToShoot) //time is a double in terms of seconds
+        {
+                shooterMotor.set(shooterSpeed);; //move robot at this speed with/without curve
+        }
+        //Timer.delay(0.005); //does nothing for 5 seconds but helps refresh motors in loop
+    }
+    //move out of tarmac
+
+    Timer.start() //starts a timer at 0 seconds
+      while(isAutonomous() && isEnabled()) //run when autonomous is Clive and you are enabled
+      {
+          if(Timer.get() < timeToGetToOutOfTarmac) //time is a double in terms of seconds
+          {
+              driveTrain.set(speed); //move robot at this speed with/without curve
+          }
+          //Timer.delay(0.005); //does nothing for 5 seconds but helps refresh motors in loop
+      }
+      Timer.stop() //stops timer
+
+
+}
